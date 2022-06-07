d8dc34eb601ea5ce047cb7a20bf967bf5fc333dc
juanpradana <suarahp@gmail.com>
2022-06-07 16:49:02 +0700

    add future site detail and get all site data

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const config = require('./config');
 const TelegramBot = require('node-telegram-bot-api');
-const request = require('request');
+const fetch = require('node-fetch');
 
 const {
   tToken,
   url_data
 } = config.config;
+
+const chunk = (arr, size) =>
+  Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+    arr.slice(i * size, i * size + size)
+  );
 
 const token = tToken;
 const bot = new TelegramBot(token, {polling: true});
@@ -13,7 +18,7 @@ const bot = new TelegramBot(token, {polling: true});
 bot.onText(/\/start/, (msg) => {
   bot.sendMessage(msg.chat.id, "Welcome", {
   "reply_markup": {
-      "keyboard": [["All Site ID", "Second sample"],   ["Keyboard"], ["I'm robot"]]
+      "keyboard": [["All Site ID"], ["Command"], ["I'm robot"]]
       }
   });
 });
@@ -31,7 +36,45 @@ bot.on('message', (msg) => {
       bot.sendMessage(msg.chat.id, "Yes I'm robot but not in that way!");
   }
 
-  // if (msg.text.indexOf("All Site ID") === 0) {
-    
-  // }
+  if (msg.text.indexOf("Command") === 0) {
+    bot.sendMessage(msg.chat.id, "All Site ID\n!site [siteid]");
+  }
+
+  if (msg.text.indexOf("All Site ID") === 0) {
+    fetch(url_data + 'SiteID')
+      .then( async (response) => {
+        const data = await response.json()
+        const subData = chunk(data, 500)
+        subData.forEach(dat => {
+          const text = JSON.stringify(dat)
+          const result = text.replace(/["{}\[\]]/g, '')
+          .replace(/site_id:/g, '')
+          final = result.replace(/,/g, "\n")
+          console.log(final.length)
+          bot.sendMessage(msg.chat.id, final)
+        })
+      })
+      .catch(function(err) {
+        console.log(err)
+        bot.sendMessage(msg.chat.id, String(err))
+      })
+  }
+
+  if (msg.text.includes("!site")) {
+    const req = msg.text.toString().replace('!site ', '')
+    fetch(url_data + 'datas/' + req)
+      .then( async (response) => {
+        const dataJSON = await response.json()
+        const data = dataJSON[0]
+        const text = JSON.stringify(data)
+        const result = String(text).replace(/,/g, '\n').replace(/[{}"]/g, '')
+        console.log(result.length)
+        bot.sendMessage(msg.chat.id, result)
+        bot.sendLocation(msg.chat.id, parseFloat(data.lat), parseFloat(data.long))
+      })
+      .catch(function(err) {
+        console.log(err)
+        bot.sendMessage(msg.chat.id, String(err))
+      })
+  }
 });
